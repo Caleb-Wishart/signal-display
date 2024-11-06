3c68e5b608573115252160a039682967da4286c3
Caleb Wishart <caleb.wish@outlook.com>
2024-11-06 10:15:07 +1000

    fix: Clearing surface no longer removes internally
    Also handle importing a surface

--- scripts/surface.lua
+++ scripts/surface.lua
@@ -24,7 +24,7 @@ function sigd_surface.surface_has_players(surface)
 end
 
 -- register a new surface when created
---- @param e EventData.on_surface_created
+--- @param e EventData.on_surface_created|EventData.on_surface_imported
 local function on_surface_created(e)
     local surface = game.get_surface(e.surface_index)
     if not surface then return end
@@ -35,18 +35,25 @@ local function on_surface_created(e)
     storage.surfaces[surface.index] = sigd_surface.surface_has_players(surface)
 end
 
--- remove a surface when deleted or cleared
---- @param e EventData.on_pre_surface_cleared|EventData.on_pre_surface_deleted
+-- remove a surface when deleted
+--- @param e EventData.on_pre_surface_deleted
 local function on_surface_deleted(e)
     if not e.surface_index then return end
     storage.surfaces[e.surface_index] = nil
     storage.displays[e.surface_index] = nil
 end
 
+-- remove all entities from a cleared surface
+--- @param e EventData.on_pre_surface_cleared
+local function on_surface_cleared(e)
+    if not e.surface_index then return end
+    storage.displays[e.surface_index] = nil
+end
 
 sigd_surface.events = {
     [defines.events.on_surface_created] = on_surface_created,
-    [defines.events.on_pre_surface_cleared] = on_surface_deleted,
+    [defines.events.on_surface_imported] = on_surface_created,
+    [defines.events.on_pre_surface_cleared] = on_surface_cleared,
     [defines.events.on_pre_surface_deleted] = on_surface_deleted,
 }
 
