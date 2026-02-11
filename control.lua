da32b41564485e03c6f5ef31c640f509252edceb
Caleb Wishart <caleb.wish@outlook.com>
2026-02-11 19:45:27 +1000

    feat: Update entity on GUI close
    Addresses some concerns in #4 where removing a value in GUI means the
    signal value won't be shown unless the value changes due to caching.
    
    Now we monitor the GUI close event and if the GUI belongs to a display
    or speaker, we invalidate the cache for that entity and force an update
    to reflect any changes.

--- control.lua
+++ control.lua
@@ -13,16 +13,30 @@ handler.add_libraries({
 -- The Event Handler library does not support events with filters, so we have to use the vanilla API for these events.
 
 local filter =
-    { { filter = "type", type = "display-panel" }, { mode = "or", filter = "type", type = "programmable-speaker" } }
+{ { filter = "type", type = "display-panel" }, { mode = "or", filter = "type", type = "programmable-speaker" } }
+-- Handle display creation, including cloning and revival
 script.on_event(defines.events.on_built_entity, sigd_display.on_display_created, filter)
 script.on_event(defines.events.on_robot_built_entity, sigd_display.on_display_created, filter)
 script.on_event(defines.events.on_space_platform_built_entity, sigd_display.on_display_created, filter)
 script.on_event(defines.events.on_entity_cloned, sigd_display.on_display_created, filter)
 script.on_event(defines.events.script_raised_revive, sigd_display.on_display_created, filter)
 script.on_event(defines.events.script_raised_built, sigd_display.on_display_created, filter)
-
+-- Handle display deletion, including mining and destruction
 script.on_event(defines.events.on_player_mined_entity, sigd_display.on_display_deleted, filter)
 script.on_event(defines.events.on_robot_mined_entity, sigd_display.on_display_deleted, filter)
 script.on_event(defines.events.on_space_platform_mined_entity, sigd_display.on_display_deleted, filter)
 script.on_event(defines.events.on_entity_died, sigd_display.on_display_deleted, filter)
 script.on_event(defines.events.script_raised_destroy, sigd_display.on_display_deleted, filter)
+
+-- Capture Display or Speaker GUI closure to ensure it is updated
+script.on_event(defines.events.on_gui_closed, function(e)
+    if e.entity and e.entity.valid and (e.entity.type == "display-panel" or e.entity.type == "programmable-speaker") then
+        -- Invalidate the cache so it forces and update even if signals haven't changed
+        -- Player may have removed previous values
+        if storage.display_signals and storage.display_signals[e.entity.unit_number] then
+            storage.display_signals[e.entity.unit_number] = {}
+        end
+        sigd_display.update_display(e.entity)
+    end
+end
+)
